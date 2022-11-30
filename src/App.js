32720e49fd6c0c273e17b223e977de9588c54aab
kaan <kaanmehmetgursoy@gmail.com>
2022-12-01 01:14:53 +0300

    test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Heart from "./components/Heart.component";
+import ILoveYou from "./components/ILoveYou.component";
+import Welcome from "./components/Welcome.component";
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome/>
+      <Heart/>
+      <ILoveYou/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ILoveYou.component.jsx
@@ -0,0 +1,9 @@
+const ILoveYou = () => {
+    return(
+        <div className="flex mt-10 justify-center items-center">
+            <h1 className="text-7xl">I Love You</h1>
+        </div>
+    )
+}
+
+export default ILoveYou
