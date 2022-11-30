9bc8c62549b3bcbad5a39ecfb63b449225970fec
kaan <kaanmehmetgursoy@gmail.com>
2022-12-01 01:23:54 +0300

    Updates

--- src/components/ILoveYou.component.jsx
+++
@@ -1,9 +0,0 @@
-const ILoveYou = () => {
-    return(
-        <div className="flex mt-10 justify-center items-center">
-            <h1 className="text-7xl">I Love You</h1>
-        </div>
-    )
-}
-
-export default ILoveYou
--- src/components/Welcome.component.jsx
+++
@@ -1,9 +0,0 @@
-const Welcome = () => {
-    return(
-        <div className="flex mt-40 justify-center items-center">
-            <h1 className="text-7xl">Welcome My Love</h1>
-        </div>
-    )
-}
-
-export default Welcome
